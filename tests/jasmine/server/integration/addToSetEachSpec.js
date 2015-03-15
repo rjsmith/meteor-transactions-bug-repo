e44b4496dc604e515d4ea1eef6de4ff0bf197c35
Richard Smith <rsmith@rsbatechnology.co.uk>
2015-03-15 14:12:28 +0000

    Added $addToSet tests

---
+++ tests/jasmine/server/integration/addToSetEachSpec.js
@@ -0,0 +1,304 @@
+/* Copyright (c) RSBA Technology Ltd 2015 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+'use strict';
+
+/**
+ * Tests for support of Mongo $addToSet + $each modifier in meteor-transactions
+ */
+Jasmine.onTest(function() {
+  describe('$addToSet', function () {
+    var transaction_id, insertedFooDoc;
+
+    beforeEach(function () {
+      // Fake userId
+      spyOn(Meteor,'userId').and.returnValue(true);
+
+      tx.start('insert foo');
+      fooCollection.insert(
+        {foo: [{bar: 1}, {bar: 2}, {bar: 3}]}, {tx: true});
+      tx.commit();
+
+      insertedFooDoc = fooCollection.findOne({foo: {$exists: true}});
+      expect(insertedFooDoc.transaction_id).toBeDefined();
+      transaction_id = insertedFooDoc.transaction_id; 
+    });
+
+    afterEach(function () {
+      fooCollection.remove({});
+      tx.Transactions.remove({});
+    });
+   
+    it ('can be updated with $addToSet modifier', function () {
+      // SETUP
+      // EXECUTE
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {
+              bar: 4
+            }
+          }
+        },
+        {
+          tx: true
+        });
+      tx.commit();
+      
+      // VERIFY
+      var fooCursor = fooCollection.find(
+      {foo: {$elemMatch: {bar: 4}}});
+      expect(fooCursor.count()).toBe(1);
+      var recoveredFoo = fooCursor.fetch()[0];
+
+      // Check transaction
+      var txDoc = tx.Transactions.findOne({_id: recoveredFoo.transaction_id});
+      console.log(JSON.stringify(txDoc));
+      expect(txDoc.items.updated[0].inverse).toEqual(
+        { command: '$set', data: [ { key: 'foo', value: [ { bar: 1 }, { bar: 2 }, { bar: 3 } ] } ] }
+        );
+      
+    })
+
+    it ('can be updated with $addToSet modifier and then undone and redone', function () {
+      // SETUP
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {
+              bar: 4
+            }
+          }
+        },
+        {
+          tx: true
+        });
+      tx.commit();
+
+      // EXECUTE
+      tx.undo();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+      {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(0);
+
+      // EXECUTE
+      tx.redo();
+      // VERIFY
+      // 
+      var fooCursor = fooCollection.find(
+        {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(1);
+      
+    })
+
+
+    it ('can be updated with $addToSet modifier using $each', function () {
+      // SETUP
+      var newBars = [{bar: 4}, {bar: 5}];
+
+      // EXECUTE
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {$each: newBars}
+          }
+        },
+        {tx: true});
+      tx.commit();
+      
+      // VERIFY
+      var fooCursor = fooCollection.find(
+        {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(1);      
+      var recoveredFoo = fooCursor.fetch()[0];
+
+      // Check transaction
+      var txDoc = tx.Transactions.findOne({_id: recoveredFoo.transaction_id});
+      expect(txDoc.items.updated[0].update).toEqual({ command: '$addToSet', data: [ { key: 'foo', value: { '＄each': [ { bar: 4 }, { bar: 5 } ] } } ] });
+    })
+
+    it ('can be updated with $addToSet modifier using $each and then undone and redone', function () {
+      // SETUP
+      var newBars = [{bar: 4}, {bar: 5}];
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {$each: newBars}
+          }
+        },
+        {tx: true});
+      tx.commit();
+
+      // EXECUTE
+      tx.undo();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+        {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(0);      
+
+      // EXECUTE
+      tx.redo();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+        {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(1);      
+
+    })
+
+    it ('can be updated with $addToSet modifier with multiple fields', function () {
+      // SETUP
+      var newBars = [{bar: 4}, {bar: 5}];
+
+      // EXECUTE
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {$each: newBars},
+            fooBar: {fooBar: 1}
+          }
+        },
+        {tx: true});
+      tx.commit();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+        {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(1);      
+      var recoveredFoo = fooCursor.fetch()[0];
+      expect(recoveredFoo.fooBar).toEqual([{fooBar: 1}]);
+      // Check transaction
+      var txDoc = tx.Transactions.findOne({_id: recoveredFoo.transaction_id});
+      expect(txDoc.items.updated[0].update).toEqual({"command":"$addToSet","data":[{"key":"foo","value":{"＄each":[{"bar":4},{"bar":5}]}},{"key":"fooBar","value":{"fooBar":1}}]});
+      expect(txDoc.items.updated[0].inverse).toEqual({"command":"$set","data":[{"key":"foo","value":[{"bar":1},{"bar":2},{"bar":3}]},{"key":"fooBar","value":[]}]});
+    });
+
+    it ('can be updated with $addToSet modifier with multiple fields and then undone and redone', function () {
+      // SETUP
+      var newBars = [{bar: 4}, {bar: 5}];
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {$each: newBars},
+            fooBar: {fooBar: 1}
+          }
+        },
+        {tx: true});
+      tx.commit();
+
+      // EXECUTE
+      tx.undo();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+        {foo: {bar: 4}, fooBar: []});
+      expect(fooCursor.count()).toBe(0);
+
+      // EXECUTE
+      tx.redo();
+
+      fooCursor = fooCollection.find(
+        {foo: {bar: 4}});
+      expect(fooCursor.count()).toBe(1);      
+      var recoveredFoo = fooCursor.fetch()[0];
+      expect(recoveredFoo.fooBar).toEqual([{fooBar: 1}]);
+     
+    });
+
+    it ('can be updated with $addToSet and $set modifiers', function () {
+      // SETUP
+      // EXECUTE
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {
+              bar: 4
+            }
+          },
+          $set: {
+            fooBar: 'barFoo'
+          }
+        },
+        {
+          tx: true
+        });
+      tx.commit();
+      
+      // VERIFY
+      var fooCursor = fooCollection.find(
+      {foo: {$elemMatch: {bar: 4}}});
+      expect(fooCursor.count()).toBe(1);
+      var recoveredFoo = fooCursor.fetch()[0];
+      expect(recoveredFoo.fooBar).toEqual('barFoo');
+
+      // Check transaction
+      var txDoc = tx.Transactions.findOne({_id: recoveredFoo.transaction_id});
+      console.log(JSON.stringify(txDoc));
+      expect(txDoc.items.updated.length).toBe(2);
+      expect(txDoc.items.updated[0].inverse).toEqual(
+        {"command":"$set","data":[{"key":"foo","value":[{"bar":1},{"bar":2},{"bar":3}]}]}
+        );
+      expect(txDoc.items.updated[1].inverse).toEqual(
+        {"command":"$unset","data":[{"key":"fooBar","value":""}]}
+        );
+    })
+
+    it ('can be updated with $addToSet and $set modifiers then undone and redone', function () {
+      // SETUP
+      tx.start('update foo');
+      fooCollection.update(
+        {_id: insertedFooDoc._id},
+        {
+          $addToSet: {
+            foo: {
+              bar: 4
+            }
+          },
+          $set: {
+            fooBar: 'barFoo'
+          }
+        },
+        {
+          tx: true
+        });
+      tx.commit();
+
+      // EXECUTE
+      tx.undo();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+      {foo: {bar: 4}, fooBar: 'barFoo'});
+      expect(fooCursor.count()).toBe(0);
+ 
+      // EXECUTE
+      tx.redo();
+
+      // VERIFY
+      var fooCursor = fooCollection.find(
+      {foo: {bar: 4}, fooBar: 'barFoo'});
+      expect(fooCursor.count()).toBe(1);
+
+     })
+
+  });
+   
+})
